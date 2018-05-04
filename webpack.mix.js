d89f93bfebe38fb13ecc28bf8f462ffedb72e211
Binh Vu Quang <binhvq@imt-soft.com>
2018-05-04 22:15:25 +0700

    + add init  files

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+const {mix} = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js(['resources/assets/js/vendor.js'], 'public/js')
+    .sass('resources/assets/sass/admin.scss', 'public/css')
+    .sass('resources/assets/sass/user.scss', 'public/css')
+    .scripts(['resources/assets/js/admin.js'], 'public/js/admin.js')
+    .scripts(['resources/assets/js/user.js'], 'public/js/user.js');
