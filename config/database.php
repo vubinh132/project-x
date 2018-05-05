69f53d6d498e67b215ec6c8438a66b16f0a50632
Binh Vu Quang <binhvq@imt-soft.com>
2018-05-05 11:39:26 +0700

    -update

--- config/database.php
+++ config/database.php
@@ -56,6 +56,8 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'strict' => true,
+            'engine' => null,
         ],
 
         'pgsql' => [
