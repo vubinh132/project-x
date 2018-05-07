3f4f375d092d3ed744d0835918ce3b91e78f2ebf
Binh Vu Quang <binhvq@imt-soft.com>
2018-05-07 09:21:19 +0700

    -revert config

--- config/database.php
+++ config/database.php
@@ -1,12 +1,5 @@
 <?php
 
-$url = parse_url(getenv("JAWSDB_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -46,13 +39,14 @@ return [
             'prefix' => '',
         ],
 
-
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
