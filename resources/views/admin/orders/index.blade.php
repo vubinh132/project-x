218bc477c5ec76b8a517703cd2953363bff58889
Binh Vu Quang <binhvq@imt-soft.com>
2018-05-10 14:07:55 +0700

    -update

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -69,7 +69,7 @@
                     <tr ng-repeat="x in filteredOrders|orderBy:'created_at' : true" on-finish-render="ngRepeatFinished">
                         <td ng-bind="x.code"></td>
                         <td ng-bind="x.statusText"></td>
-                        <td ng-bind-html="trustAsHtml(x.totalPrice)" data-toggle='tooltip' ng-attr-title="{%x.orderDetail%}" data-html="true" data-animation="false" bs-tooltip></td>
+                        <td><span  ng-bind-html="trustAsHtml(x.totalPrice)" data-toggle='tooltip' ng-attr-title="{%x.orderDetail%}" data-html="true" data-animation="false" bs-tooltip></span></td>
                         <td ng-bind="x.name"></td>
                         <td ng-bind="x.sellingWeb"></td>
                         <td class="text-center text-nowrap">
